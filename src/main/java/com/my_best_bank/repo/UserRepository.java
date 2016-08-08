29d41b54cb77e06147e91d2478d0a589f63e6e10
Serhei Brahin <s.brahin@aimprosoft.com>
2016-08-08 17:15:35 +0300

    Simple websecurity

--- src/main/java/com/my_best_bank/repo/UserRepository.java
+++ src/main/java/com/my_best_bank/repo/UserRepository.java
@@ -3,7 +3,9 @@ package com.my_best_bank.repo;
 
 import com.my_best_bank.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     User findByEmail(String email);
 }
