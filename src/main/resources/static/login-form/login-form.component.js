3edb9a1c57e2bb8c3a74c150360db523d478d8b4
Serhei Brahin <s.brahin@aimprosoft.com>
2016-08-10 16:38:49 +0300

    Simple JWT, and angular

--- src/main/resources/static/login-form/login-form.component.js
+++ src/main/resources/static/login-form/login-form.component.js
@@ -1,69 +1,36 @@
 angular
     .module('loginForm')
     .component('loginForm', {
-        templateUrl: 'login-form/login-form.template.html',
-        controller:['$routeParams', 'userService', '$scope',
-            function loginFormController(userService, $scope) {
-
-               /* $scope.token = null;
-                $scope.error = null;
-
-                $scope.login = function () {
-                    $scope.error = null;
-                    userService.login($scope.userEmail).then(function (token) {
-                            $scope.token = token;
-                            $http.defaults.headers.common.Authorization = 'Bearer ' + token;
-                        },
-                        function (error) {
-                            $scope.error = error;
-                            $scope.userEmail = ''
-                        }
-                    )
-                }
-
-                $scope.logout = function () {
-                    $scope.userEmail = '';
-                    $scope.token = null;
-                    $http.defaults.headers.common.Authorization = '';
-                }
-
-                $scope.loggedIn = function() {
-                    return $scope.token !== null;
-                }
-
-           */
-        }]
+        templateUrl: 'login-form/login-form.template.html'
     });
-angular.module('loginForm').controller('UserCtrl', ['userService','$scope','$http',
-    function(mainService, $scope, $http) {
+
+angular.module('loginForm').controller('UserCtrl', ['userService', '$scope', '$http',
+    function (mainService, $scope, $http) {
 
         $scope.token = null;
         $scope.error = null;
 
-        $scope.login = function() {
+        $scope.login = function () {
             $scope.error = null;
-            mainService.login($scope.userEmail).then(function(token) {
+            mainService.login($scope.userEmail).then(function (token) {
                     $scope.token = token;
                     $http.defaults.headers.common.Authorization = 'Bearer ' + token;
                 },
-                function(error){
+                function (error) {
                     $scope.error = error;
                     $scope.userEmail = '';
                 });
         };
 
-        $scope.logout = function() {
+        $scope.logout = function () {
             $scope.userEmail = '';
             $scope.token = null;
             $http.defaults.headers.common.Authorization = '';
         };
 
-        $scope.loggedIn = function() {
+        $scope.loggedIn = function () {
             return $scope.token !== null;
         }
     }
 ]);
-/*
- function axaxaFunction() {
- this.hiMan = "HI man!";
- }*/
+
